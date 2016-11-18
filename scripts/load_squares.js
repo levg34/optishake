ca847cbfec6d071e0428c709dd163dc17706d3e5
levg34 <guilhem.dross@laposte.net>
2016-11-18 15:57:35 +0100

    some modifs

--- scripts/load_squares.js
+++ scripts/load_squares.js
@@ -4,23 +4,23 @@ var w = 960,
 	x = w / z,
 	y = h / z;
 
-var svg = d3.select("body").append("svg")
-	.attr("width", w)
-	.attr("height", h);
+var svg = d3.select('body').append('svg')
+	.attr('width', w)
+	.attr('height', h);
 
-svg.selectAll("rect")
+svg.selectAll('rect')
 	.data(d3.range(x * y))
-	.enter().append("rect")
-	.attr("transform", translate)
-	.attr("width", z)
-	.attr("height", z)
-	.style("fill", "gray")
-	.on("click", mouseover);
+	.enter().append('rect')
+	.attr('transform', translate)
+	.attr('width', z)
+	.attr('height', z)
+	.style('fill', 'gray')
+	.on('click', mouseover);
 
 function translate(d) {
-	return "translate(" + (d % x) * z + "," + Math.floor(d / x) * z + ")";
+	return 'translate(' + (d % x) * z + ',' + Math.floor(d / x) * z + ')';
 }
 
 function mouseover(d) {
-	d3.select(this).style("fill", "red")
+	d3.select(this).style('fill', 'black')
 }
